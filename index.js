30e6982729349023ef7dd99ce4a125a9e01d629e
onedayherenow <onedayherenow@gmail.com>
2022-09-26 14:42:14 -0400

    I am in Love !!!

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 // 1. Declare global variable to store the web3 instance
-let PetContract;
+let MemberContract;
 
 // 2. Set contract address and ABI
-const Pet_Contract_Address = "";
-const Pet_Contract_ABI = [];
+const Member_Contract_Address = "0xc663e48592E6dE1a37843410777886D2255Cc15c";
+const Member_Contract_ABI = [];
 
 // 3. Prompt user to sign in with MetaMask
 const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
@@ -14,10 +14,22 @@ const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
             const signer = provider.getSigner(accounts[0]);
 
             /* 3.1 Create instance of pet smart contract */
-            PetContract = new ethers.Contract(
-                Pet_Contract_Address,
-                Pet_Contract_ABI,
+            MemberContract = new ethers.Contract(
+                Member_Contract_Address,
+                Member_Contract_ABI,
                 signer
             );
         });
     });
+
+
+    // 4. Creating variables for reusable dom elements
+const memberFormSection = document.querySelector(".pet-form-section");   // selects these elements by their class name, and assigns it to a variable
+const showMemberFormBtn = document.querySelector(".show-pet-form-button");
+const memberSection = document.querySelector(".member-detail-section");
+const setMemberButton = document.querySelector("#set-new-member");
+const refreshBtn = document.querySelector(".refresh-pet-details-btn");
+
+
+
+// 5. Function to set member details
